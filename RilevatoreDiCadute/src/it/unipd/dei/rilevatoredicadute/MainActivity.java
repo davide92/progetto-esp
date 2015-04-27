95848039b1473506ba6ae974ce83db7ef7c3388c
davide <davide@davide-N53SM>
2015-04-27 10:43:10 +0200

    ui3 + modifiche ui1 e ui2

--- RilevatoreDiCadute/src/it/unipd/dei/rilevatoredicadute/MainActivity.java
+++ RilevatoreDiCadute/src/it/unipd/dei/rilevatoredicadute/MainActivity.java
@@ -3,13 +3,9 @@ package it.unipd.dei.rilevatoredicadute;
 import java.util.LinkedList;
 import java.util.List;
 import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 
@@ -26,15 +22,10 @@ public class MainActivity extends ActionBarActivity {
         list.add(new Dati());
         CustomAdapter adapter = new CustomAdapter(this, R.layout.list_items, list);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new OnItemClickListener(){
-        public void onItemClick(AdapterView<?> adapter, View v, int position, long id){
-        	//Dati value = (Dati)adapter.getItemAtPosition(position);
-        	Intent UI2 = new Intent(getApplicationContext(), Second.class);
-        	startActivity(UI2);
-        	}
-        });
-        
+                    
 	}
+     
+	
 	
     public boolean onCreateOptionsMenu(Menu menu){
     	super.onCreateOptionsMenu(menu);
@@ -42,8 +33,6 @@ public class MainActivity extends ActionBarActivity {
     	MenuItem meIt2 = menu.add(0, R.id.delete, 2, "Cancella");
     	MenuItem meIt3 = menu.add(0, R.id.rinomina, 3, "Rinomina");
     	MenuItem meIt4 = menu.add(0, R.id.preferenze, 4, "Preferenze");
-    	meIt1.setIntent(new Intent(this, Third.class));
-    	//meIt4.setIntent(new Intent(this, Fifth.class))
     	return true;
     } 
     
