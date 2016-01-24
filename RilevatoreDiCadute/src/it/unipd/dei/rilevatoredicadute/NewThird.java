c1917737fea9fb74d6cf6e0d8808b823cd61d742
EmanueleRiso <lu_risu@libero.it>
2016-01-24 15:04:29 +0100

    Nuova NewThird

--- RilevatoreDiCadute/src/it/unipd/dei/rilevatoredicadute/NewThird.java
+++ RilevatoreDiCadute/src/it/unipd/dei/rilevatoredicadute/NewThird.java
@@ -24,12 +24,10 @@ import android.content.ComponentName;
 import android.util.Log;
 import android.graphics.Color;
 import android.database.Cursor;
-
 import java.util.GregorianCalendar;
 import java.util.Random;
 import java.util.LinkedList;
 import java.util.List;
-
 import it.unipd.dei.rilevatoredicadute.ServiceCronometro.MyBinder;
 import it.unipd.dei.rilevatoredicadute.FindFall.MyBinderText;
 
@@ -38,7 +36,7 @@ public class NewThird extends ActionBarActivity{
 	
 	GregorianCalendar cal;
 	MyDBManager db;
-	public long stopTime, pauseTime =0;		
+	public long stopTime=0;		
 	private  ListView listView;
 	private List<DatiCadute> fallList;//lista delle cadute temporanea 
 	long it = 0;	
@@ -54,7 +52,7 @@ public class NewThird extends ActionBarActivity{
 	Intent mService = null;
 	CountDownTimer cdCrono = null; //conto alla rovescia perla visualizzazione dati del cronometro
 	CountDownTimer cdText = null; //conto alla rovescia per la visualizzazione dati accelerometro
-	/*memorizza stato della sessione
+	/*memorizza stato della sessione, valore variabile SNT
 	  0 -> activity stoppata
 	  1 -> activity avviata
 	  2 -> activity in pausa
@@ -74,13 +72,12 @@ public class NewThird extends ActionBarActivity{
 	Intent T;	
 	Intent intent;
 	Intent TextIntent;
-	MyReceiver myReceiver;   //Receiver per aggiornare la durata della sessione
+	ReceiverTextViewDurataSessione RTVDS;
 	CustomAdapterFalls adapter;	
-	Receiver receiver; //Receiver per l'aggiornamento delle informazioni su una caduta durante una sessione
-	TextReceiver textReceiver; //Receiver per l'aggiornamento dei dati dell'accelerometro
+	ReceiverVibrazioneUpdateUI RVUUI;		
+	ReceiverTestoValoriAccelerometro RTVA;
 	float [] arrayRicevuto = new float[3];
 	boolean VarSavedInstance = false; //variabile per sapere se è stata salvata l'istanza dell'activity
-	boolean nRS = true;//rileva se lo schermo non è stato ruotato
 	
 	@Override
 	protected void onCreate(Bundle savedInstance) {		
@@ -91,54 +88,50 @@ public class NewThird extends ActionBarActivity{
 		fallList = new LinkedList<DatiCadute>();
 		adapter = new CustomAdapterFalls(this, R.id.listViewCadute, fallList);       
 		listView.setAdapter(adapter);
-		cal= new GregorianCalendar();		
-				
+		cal= new GregorianCalendar();
+		
+		/*T = getIntent();
+		s = T.getIntExtra(MainActivity.PACKAGE_NAME+".statoSessione", 3);
+		Log.v("TAG_MA","VALORE VARIABILE s: "+ s);*/
+		
 		timestampText = (TextView) findViewById(R.id.timestamp_text);
 		playBtn = (ImageButton)findViewById(R.id.start);
 		pauseBtn = (ImageButton)findViewById(R.id.pause);
-		stopBtn = (ImageButton)findViewById(R.id.stop);
+		stopBtn = (ImageButton)findViewById(R.id.stop);		
 		//inizializzazione dello stato della sessione
-		if((savedInstance != null)){	
+		if((savedInstance !=null)){	
 			VarSavedInstance = true;
-			Log.v("Valore variabile VarSavedInstance","" + VarSavedInstance);
 			Log.v("<<SAVED INSTANCE ON>>", "---SAVED INSTANCE ON---");			
 			StampaDurataService = savedInstance.getString("durataCrono");			
 			SNT = savedInstance.getInt("statesession");			
 			data = savedInstance.getString("data");
 			stopTime = savedInstance.getLong("TempoPausa");
-			pauseTime = savedInstance.getLong("tempoRotazione");
 			NS = savedInstance.getString("nomeSessione");
-			Log.v("Valore variabile NS","" +NS);
 			cStart = savedInstance.getInt("cMs");
-			nRS = savedInstance.getBoolean("nRS");
-			nRS = true;			
-		}
-		Log.v("Valore variabile nRS","" + nRS);
+			//s = savedInstance.getInt("statoSessioneControllo");
+			Log.v("TAG_NT_ONSAVEINSTANCE","valore statoSessione dopo:"+SNT);
+		}				
 	}
 	//salvataggio dello stato della sessione in corso
 	@Override
-	protected void onSaveInstanceState(Bundle savedInstance) {			
+	protected void onSaveInstanceState(Bundle savedInstance) {	
+		//savedInstance.putInt("statoSessioneControllo",s);
 	    savedInstance.putInt("statesession", SNT);	    
 	    savedInstance.putString("data", date);
 	    savedInstance.putString("durataCrono", StampaDurataService);	    
 	    savedInstance.putLong("TempoPausa", stopTime);
-	    pauseTime = SystemClock.elapsedRealtime();
-	    savedInstance.putLong("tempoRotazione", pauseTime);
 	    savedInstance.putString("nomeSessione", NS);
-	    Log.v("Valore variabile NS", NS);
 	    savedInstance.putInt("cMs", cStart);
-	    Log.v("Valore variabile nRS","" +nRS);
-	    nRS = false;
-	    savedInstance.putBoolean("nRS", nRS);
-	    Log.v("Valore variabile nRS","" +nRS);
-	    Log.v("<<ATTENZIONE>>","CAMBIO ORIENTAZIONE TELEFONO");
+	    Log.v("TAG_NT_ONSAVEINSTANCE","valore statoSessione prima:"+SNT);
 	    super.onSaveInstanceState(savedInstance);	    
 	}
 	
 	@Override
 	protected void onStart(){
 		super.onStart();
-		Log.v("TAG", "----INIZIO-THIRD----");
+		Log.v("TAG_NT", "----INIZIO THIRD----");
+		Log.v("TAG_NT", "onStart valore variabile s: "+s);
+		
 		//COUNT DOWN TIMER PER LA GESTIONE DELLA TEXTVIEW CHE VISUALIZZA LA DURATA DELLA SESSIONE
 		cdCrono = new CountDownTimer(1000L, 100L) {					
 					@Override
@@ -176,34 +169,39 @@ public class NewThird extends ActionBarActivity{
 					}
 		};
 		
-		myReceiver = new MyReceiver();
-		receiver = new Receiver();
-		textReceiver = new TextReceiver();
+		RTVDS = new ReceiverTextViewDurataSessione();
+		RVUUI = new ReceiverVibrazioneUpdateUI();
+		RTVA = new ReceiverTestoValoriAccelerometro();
+		
 		IntentFilter intentFilter = new IntentFilter();
 		intentFilter.addAction(ServiceCronometro.MY_ACTION);
-		IntentFilter filter = new IntentFilter();
+		
+		IntentFilter filter = new IntentFilter();		
 		filter.addAction(FindFall.BROADCAST);
+		
 		IntentFilter TextFilt = new IntentFilter();
 		TextFilt.addAction(FindFall.TEXTVIEW);
-		registerReceiver(myReceiver,intentFilter);
-		registerReceiver(receiver, filter);
-		registerReceiver(textReceiver, TextFilt);
+		
+		registerReceiver(RTVDS,intentFilter);
+		registerReceiver(RVUUI, filter);
+		registerReceiver(RTVA, TextFilt);
 		
 		db = new MyDBManager(this);	
 		PACKAGE_NAME = getApplicationContext().getPackageName();
 		T = getIntent(); 
-		
-		if((!VarSavedInstance) && SNT != 1){
-			NS = T.getStringExtra(MainActivity.PACKAGE_NAME+".nomeSessione");
-			Log.v("null","" +NS);			        
+		if(!(VarSavedInstance)){
+			NS = T.getStringExtra(MainActivity.PACKAGE_NAME+".nomeSessione");			
 		}
 		s = T.getIntExtra(MainActivity.PACKAGE_NAME+".statoSessione", 3);
+		Log.v("TAG_MA","VALORE VARIABILE s: "+ s);
+		Log.v("TAG_MA","VALORE VARIABILE SNT: "+ SNT);
+		Log.v("TAG_MA","VALORE VARIABILE stopTime: "+stopTime);
 		
 		xAccViewS = (TextView) findViewById(R.id.xDataS);
 		yAccViewS = (TextView) findViewById(R.id.yDataS);
 		zAccViewS = (TextView) findViewById(R.id.zDataS);
 		tx = (TextView)findViewById(R.id.TestoSessCurr);        
-        tx.setText(NS);    
+        tx.setText(NS);
         
         if(s == 1 && SNT ==2 ){
         	playBtn.setVisibility(View.VISIBLE);
@@ -214,25 +212,19 @@ public class NewThird extends ActionBarActivity{
         		playBtn.setVisibility(View.INVISIBLE);
 				pauseBtn.setVisibility(View.VISIBLE);
 				cStart++;
+				cdText.start();				
         	}else{
+        		if(s == 2 && SNT == 2){
+        			playBtn.setVisibility(View.VISIBLE);
+        			pauseBtn.setVisibility(View.INVISIBLE);
+        			//stopTime = T.getLongExtra(MainActivity.PACKAGE_NAME+".PausaTempo", 0);
+        		}else{
         		if(s == 3){//GESTIONE ORIENTAZIONE TELEFONO
         			if(SNT == 1){				
         				playBtn.setVisibility(View.INVISIBLE);
         				pauseBtn.setVisibility(View.VISIBLE);
         				cStart++;
-        				cdText.start();	
-        				intent = new Intent(getApplicationContext(), ServiceCronometro.class);
-        				if ( pauseTime != 0 ){
-        					long intervalloPausa = (SystemClock.elapsedRealtime() - pauseTime);
-        					intent.putExtra("pausa",intervalloPausa);
-        				}	
-        				TextIntent = new Intent(getApplicationContext(), FindFall.class);
-        				TextIntent.putExtra("nome sessione", NS);
-        				startService(intent);
-        				bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);				
-        				startService(TextIntent);
-        				bindService(TextIntent, mServiceConnectionText, Context.BIND_AUTO_CREATE);
-        				cdText.start();
+        				cdText.start();						
         			}
         			else{				
         				playBtn.setVisibility(View.VISIBLE);
@@ -254,12 +246,32 @@ public class NewThird extends ActionBarActivity{
         		}	
         	}
         }
-        
+        }
+        //}
+        /*if(SNT == 3){
+        	playBtn.setVisibility(View.VISIBLE);
+			pauseBtn.setVisibility(View.INVISIBLE);	
+        }
+        else{
+        	if(SNT == 1){
+        		playBtn.setVisibility(View.INVISIBLE);
+				pauseBtn.setVisibility(View.VISIBLE);
+				cStart++;
+				cdText.start();
+        	}
+        	else{
+        		if(SNT == 2){
+        			playBtn.setVisibility(View.VISIBLE);
+    				pauseBtn.setVisibility(View.INVISIBLE);
+    				stopTime = T.getLongExtra(MainActivity.PACKAGE_NAME+".PausaTempo", 0);
+        		}
+        	}
+        }*/
 		intent = new Intent(getApplicationContext(), ServiceCronometro.class);		
 		TextIntent = new Intent(getApplicationContext(), FindFall.class);
 		TextIntent.putExtra("nome sessione", NS);
 		
-		cdCrono.start();
+		cdCrono.start();		
 		updateUI();//aggiornamento interfaccia utente
 		
 		
@@ -269,6 +281,7 @@ public class NewThird extends ActionBarActivity{
 				if(SNT == 2)
 					cdCrono.start();
 				SNT = 1;
+				//s = 1;
 				playBtn.setVisibility(View.INVISIBLE);
 				pauseBtn.setVisibility(View.VISIBLE);				
 				
@@ -295,14 +308,16 @@ public class NewThird extends ActionBarActivity{
 				startService(TextIntent);
 				bindService(TextIntent, mServiceConnectionText, Context.BIND_AUTO_CREATE);
 				cdText.start();
-				Log.v("List-1","--HO PREMUTO IL TASTO PLAY---");				
+				db.close();
+				
+				Log.v("Tag_NT","--HO PREMUTO IL TASTO PLAY---");				
 			}						
 		});
 		
 		pauseBtn.setOnClickListener(new View.OnClickListener(){			
 			@Override
 			public void onClick(View v){				
-				
+				//s = 2;
 				SNT = 2;
 				cStart--;
 				cdCrono.cancel();				
@@ -360,88 +375,100 @@ public class NewThird extends ActionBarActivity{
 								
 				Intent UIMA;
 			    UIMA = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(UIMA);
-				
-			}			
-			
+                startActivity(UIMA);				
+			}						
 		});		
 	}
 	
 	@Override
 	protected void onPause(){
-		super.onPause();		
+		super.onPause();
+		Log.v("TAG_NT","NT_ON_PAUSE()");
+		/*SNT = 2;
+		String ora=StampaDurataService;
+		db.aggiornaDurataSessione(ora,NS);
+		db.aggiornaStatoSessione(SNT, NS);
+		Log.v("stopSessione------->",""+NS+"");
+		//chiusura della "connessione" al service
+		if (mServiceBound) {
+			 unbindService(mServiceConnection);
+			 mServiceBound = false;
+		}
+		//chiusura della "connessione" al service
+		if(mServiceBoundText){
+			unbindService(mServiceConnectionText);
+			mServiceBoundText = false;
+		}
+		stopService(intent);
+		if(cStart >0)
+			stopService(TextIntent);
+		cdCrono.cancel();
+		cdText.cancel();
+		*/		
 	}	
 
 	@Override
 	protected void onStop(){
-		super.onStop();
+		super.onStop();	
+		//SNT = 0;
+		//db.aggiornaStatoSessione(SNT, NS);
+		Log.v("TAG_NT","NEWTHIRD STOPPED");		
+		try{
+			unregisterReceiver(RVUUI);
+			}catch(IllegalArgumentException e){						
+			}
+		try{
+			unregisterReceiver(RTVDS);
+		}catch(Exception exc){			
+		}
+		try{
+			unregisterReceiver(RTVA);
+		}catch(Exception exc){
+			
+		}
+		
+		//chiusura della "connessione" al service
+		if (mServiceBound) {
+			unbindService(mServiceConnection);
+			mServiceBound = false;
+	    }
+		//chiusura della "connessione" al service
+		if(mServiceBoundText){
+			unbindService(mServiceConnectionText);
+			mServiceBoundText = false;
+		}
 		cdCrono.cancel();
-		cdText.cancel();
+		cdText.cancel();					
 	}
 	
 	@Override
 	protected void onRestart(){
-		super.onRestart();	
+		super.onRestart();
+		Log.v("TAG_NT","NT_ON_RESTART()");
 		Log.v("NEW THIRD", "ACTIVITY NEWTHIRD RESTART  "+NS);
-		IntentFilter intentFilter = new IntentFilter();
+		/*IntentFilter intentFilter = new IntentFilter();
 		intentFilter.addAction(ServiceCronometro.MY_ACTION);
 		IntentFilter filter = new IntentFilter();
 		filter.addAction(FindFall.BROADCAST);
 		IntentFilter TextFilt = new IntentFilter();
 		TextFilt.addAction(FindFall.TEXTVIEW);
-		registerReceiver(myReceiver,intentFilter);
-		registerReceiver(receiver, filter);
-		registerReceiver(textReceiver, TextFilt);
+		registerReceiver(RTVDS,intentFilter);
+		registerReceiver(RVUUI, filter);
+		registerReceiver(RTVA, TextFilt);*/
 	}
 	
 	@Override
 	protected void onDestroy(){
 		super.onDestroy();
-		Log.v("NEW THIRD", "ACTIVITY NEWTHIRD DESTROY  "+ NS);
-		Log.v("Valore variabile nRS","" +nRS);
-		unregisterReceiver(myReceiver);
-		unregisterReceiver(receiver);
-		unregisterReceiver(textReceiver);
-		unbindService(mServiceConnection);
-		unbindService(mServiceConnectionText);
-		/*if(SNT == 1 && nRS){
-			Log.v("ACTIVITY THIRD DESTROY","avviato if");
-			SNT = 0;
-			String ora=StampaDurataService;				
-			db.aggiornaDurataSessione(ora,NS);
-			db.aggiornaStatoSessione(SNT, NS);
-			try{
-				unregisterReceiver(receiver);
-				}catch(IllegalArgumentException e){						
-				}
-			try{
-				unregisterReceiver(myReceiver);
-			}catch(Exception exc){			
-			}
-			try{
-				unregisterReceiver(textReceiver);
-			}catch(Exception exc){
-				
-			}
-			//chiusura della "connessione" al service
-			if (mServiceBound) {
-				unbindService(mServiceConnection);
-				mServiceBound = false;
-		    }
-			//chiusura della "connessione" al service
-			if(mServiceBoundText){
-				unbindService(mServiceConnectionText);
-				mServiceBoundText = false;
-			}
-		}*/
-		if (db != null) 
-	    {
+		Log.v("TAG_NT","NT_ON_DESTROY()");
+		
+		if (db != null){
 	        db.close();
 	    }
 	}
 	
 	//RECEIVER GESTIONE TEXTVIEW DURATA SESSIONE
-	private class MyReceiver extends BroadcastReceiver{		 
+	private class ReceiverTextViewDurataSessione extends BroadcastReceiver{		 
 		 @Override
 		 public void onReceive(Context arg0, Intent arg1) {
 		  // TODO Auto-generated method stub		  	  
@@ -451,7 +478,7 @@ public class NewThird extends ActionBarActivity{
 	}
 	
 	//receiver che fa vibrare il dispositivo e aggiorna l'interfaccia utente avvenuta una caduta
-	private class Receiver extends BroadcastReceiver{			
+	private class ReceiverVibrazioneUpdateUI extends BroadcastReceiver{			
 		@Override
 		public void onReceive(Context arg0, Intent arg1) {
 			boolean ricevuto = arg1.getBooleanExtra("fall", false);			
@@ -463,14 +490,15 @@ public class NewThird extends ActionBarActivity{
 		}						
 	}    
 	//receiver per la lettura dei valori dell'accelerometro
-	private class TextReceiver extends BroadcastReceiver{
+	private class ReceiverTestoValoriAccelerometro extends BroadcastReceiver{
 		@Override
 		public void onReceive(Context arg0, Intent arg1){
 			if(SNT != 2)
 				arrayRicevuto = arg1.getFloatArrayExtra("textview");//array dei valori degli assi x, y e z dell'accelerometro	
 		}
 	}
-		
+	
+	//classe serviceConnection per il service del cronometro
     private ServiceConnection mServiceConnection = new ServiceConnection() {
     	
 		 @Override
@@ -486,6 +514,7 @@ public class NewThird extends ActionBarActivity{
 		 }
 	 };		
 	 
+	 //classe serviceConnection per il service della classe findfall
 	 private ServiceConnection mServiceConnectionText= new ServiceConnection(){
 		 @Override
 		 public void onServiceDisconnected(ComponentName name) {
@@ -516,8 +545,9 @@ public class NewThird extends ActionBarActivity{
 			case(R.id.mostra):{				  
 				Intent UIMA;
 				UIMA = new Intent(getApplicationContext(), MainActivity.class);
-				Log.v("---","---");										
+				Log.v("------","-------");										
 				db.aggiornaStatoSessione(SNT, NS);
+				
 				if(SNT == 2){
 					db.inserireTempoPausaSessione(NS, stopTime);					
 				}
@@ -538,6 +568,7 @@ public class NewThird extends ActionBarActivity{
 	
 	//metodo per aggiornare la UI nel caso di una caduta rilevata
 	private void updateUI() {
+		
 		//cancellazione dei dati salvati e visualizzati precedentemente
 		if(fallList.size() > 0){
 			fallList.clear();
